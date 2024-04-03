f519a0dd2a3e995aabfcfaee2648de8eb2b45b1c
MartinCupak <30996725+MartinCupak@users.noreply.github.com>
2024-04-03 18:08:11 +0800

    add exceptions handling to wav write

--- IMOSPATools/wav.py
+++ IMOSPATools/wav.py
@@ -2,10 +2,6 @@ import wave
 from datetime import datetime
 from typing import Tuple
 
-
-class IMOSAcousticWavException(Exception):
-    pass
-
 # Write simple mono wav file
 #   !@#$%^&* Warning: assuming single channel only,
 #   eg: C0=1 C1=0 C2=0 C3=0 in the header.
@@ -19,14 +15,21 @@ def write(log, rawFileName, sampleRate, binDataSuccess, binData):
         wavFileName = rawFileName + '.wav'
 
     # Open the WAV file
-    with wave.open(wavFileName, 'w') as wavFile:
-        # Set the parameters of the output file
-        wavFile.setnchannels(1)  # mono
-        wavFile.setsampwidth(2)  # in bytes, 16bit samples
-        wavFile.setframerate(sampleRate)
-        # write audio as binary data block
-        wavFile.setnframes(binData.size)
-        wavFile.writeframes(binData.tobytes())
+    try:
+        with wave.open(wavFileName, 'w') as wavFile:
+            # Set the parameters of the output file
+            wavFile.setnchannels(1)  # mono
+            wavFile.setsampwidth(2)  # in bytes, 16bit samples
+            wavFile.setframerate(sampleRate)
+            # write audio as binary data block
+            wavFile.setnframes(binData.size)
+            wavFile.writeframes(binData.tobytes())
         
-        log.info(f"Written {wavFileName}")
+            log.info(f"Written {wavFileName}")
 
+    except wave.Error as e:
+        log.error(f"Error writing WAV file {wavFileName}: {e}")
+    except IOError as e:
+        log.error(f"Error opening WAV file {wavFileName}: {e}")
+    except Exception as e:
+        log.error(f"Unexpected error {wavFileName}: {e}")
