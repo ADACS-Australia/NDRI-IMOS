5086abd026fca14eb33469baddb3068965ab5559
MartinCupak <30996725+MartinCupak@users.noreply.github.com>
2024-04-03 18:07:43 +0800

    make --filename argument mandatory

--- scripts/dat2wav.py
+++ scripts/dat2wav.py
@@ -20,7 +20,7 @@ log = logging.getLogger('IMOSPATools')
 def parseArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument('--debug', '-d', action='store_true', help='Enable debug mode')
-    parser.add_argument('--filename', '-f', help='The name of the raw .DAT file to process.')
+    parser.add_argument('--filename', '-f', required=True, help='The name of the raw .DAT file to process.')
     args = parser.parse_args()
     return args
         
