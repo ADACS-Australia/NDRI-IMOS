fbe53d6a7a2af06516638a3df9bd507246869cc6
MartinCupak <30996725+MartinCupak@users.noreply.github.com>
2024-04-04 17:30:49 +0800

    initial ver, just skethcing func headers

---
+++ IMOSPATools/calibration.py
@@ -0,0 +1,27 @@
+import re
+import os
+import numpy
+from datetime import datetime
+from typing import Tuple
+import _io
+import logging
+
+
+class IMOSAcousticCalibException(Exception):
+    pass
+
+
+def calcOverload(binData: numpy.ndarray) -> int:
+    
+    return overload
+
+
+def toVolts(binData: numpy.ndarray) -> numpy.ndarray:
+    
+    return voltsData
+
+
+def calibrate(volts: numpy.ndarray) -> numpy.ndarray:
+    
+    
+    return calibData
