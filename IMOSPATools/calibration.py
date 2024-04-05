a98eb705b5ab42a31ba8a26f2f7fc4003958f647
MartinCupak <30996725+MartinCupak@users.noreply.github.com>
2024-04-05 18:24:22 +0800

    code of toVolts() func

--- IMOSPATools/calibration.py
+++ IMOSPATools/calibration.py
@@ -7,29 +7,45 @@ import _io
 import logging
 from typing import Final
 
-OVERLOAD_LOWER_BOUND: Final[integer] = 50
-OVERLOAD_UPPER_BOUND: Final[integer] = 65000
+from IMOSPATools import rawdat
 
+OVERLOAD_LOWER_BOUND: Final[int] = 50
+OVERLOAD_UPPER_BOUND: Final[int] = 65000
+FULLSCALE_VOLTS: Final[float] = 5.0
 
 class IMOSAcousticCalibException(Exception):
     pass
 
 
-def calculateOverload(binData: numpy.ndarray) -> int:
-    overloadCount = 0
+def countOverload(binData: numpy.ndarray) -> int:
+    """
+    Count samples with overload
+    (too sensitive gain, limited)
+    
+    :param binData: raw audio data
+    :return: count of samples with overload
+    """
+    count = 0
     for i in binData:
         if i < OVERLOAD_LOWER_BOUND or i > OVERLOAD_UPPER_BOUND:
-            overloadCount += 1
+            count += 1
 
-    return overloadCount
-
+    return count
 
 def toVolts(binData: numpy.ndarray) -> numpy.ndarray:
+    """
+    Convert waw data to Volts
+    Modified Franks method
     
+    :param binData: raw audio data
+    :return: audio data in Volts
+    """
+    # Multiply by this factor to convert A/D counts to volts 0-5
+    countsToVolts = FULLSCALE_VOLTS/rawdat.BITS_PER_SAMPLE
+    voltsData[:] = (countsToVolts * binData[:]) - np.mean(binData[:] * countsToVolts)
+
     return voltsData
 
-
 def calibrate(volts: numpy.ndarray) -> numpy.ndarray:
-    
     
     return calibData
