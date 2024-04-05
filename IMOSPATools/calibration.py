ebf21240f7350a2d069b9da0a9fd0a18cf7875e3
MartinCupak <30996725+MartinCupak@users.noreply.github.com>
2024-04-05 16:49:43 +0800

    support python 3.8+ to make use of typing.Final; and use it

--- IMOSPATools/calibration.py
+++ IMOSPATools/calibration.py
@@ -5,15 +5,23 @@ from datetime import datetime
 from typing import Tuple
 import _io
 import logging
+from typing import Final
+
+OVERLOAD_LOWER_BOUND: Final[integer] = 50
+OVERLOAD_UPPER_BOUND: Final[integer] = 65000
 
 
 class IMOSAcousticCalibException(Exception):
     pass
 
 
-def calcOverload(binData: numpy.ndarray) -> int:
-    
-    return overload
+def calculateOverload(binData: numpy.ndarray) -> int:
+    overloadCount = 0
+    for i in binData:
+        if i < OVERLOAD_LOWER_BOUND or i > OVERLOAD_UPPER_BOUND:
+            overloadCount += 1
+
+    return overloadCount
 
 
 def toVolts(binData: numpy.ndarray) -> numpy.ndarray:
