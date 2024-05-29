17bbd9a7b5394548b9ea99243441e005c6cd0796
MartinCupak <30996725+MartinCupak@users.noreply.github.com>
2024-05-29 18:00:57 +0800

    bump ver to 0.2

--- IMOSPATools/__init__.py
+++ IMOSPATools/__init__.py
@@ -17,5 +17,5 @@ https://adacs.org.au
 """
 __author__ = 'Martin Cupak'
 __email__ = 'martin.cupak@curtin.edu.au'
-__version__ = '0.1'
-__date__ = '2024-03-05'
+__version__ = '0.2'
+__date__ = '2024-05-29'
