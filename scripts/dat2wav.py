e130a966223daf47f9a73f2b126ab8213f1510d3
MartinCupak <30996725+MartinCupak@users.noreply.github.com>
2024-07-11 15:10:35 +0800

    mv diag_plot.py diagplot.py and change source code

--- scripts/dat2wav.py
+++ scripts/dat2wav.py
@@ -11,7 +11,7 @@ import numpy
 from IMOSPATools import rawdat
 from IMOSPATools import wav
 from IMOSPATools import calibration
-from IMOSPATools import diag_plot
+from IMOSPATools import diagplot
 
 log = logging.getLogger('IMOSPATools')
 calibration.doWriteIntermediateResults = False
@@ -96,8 +96,8 @@ if __name__ == "__main__":
             # WTF python you typeless language! 
             # The print defaults to float even for an explicit int16!
             numpy.savetxt('signal_final_16bit_int.txt', scaledCalibSignal, fmt='%d')
-            diag_plot.dp.add_plot(scaledCalibSignal, "Signal final 16bit")
-            diag_plot.dp.show()
+            diagplot.dp.add_plot(scaledCalibSignal, "Signal final 16bit")
+            diagplot.dp.show()
 
         # debugging...
         log.debug(f"scaled calibrated signal size is: {scaledCalibSignal.size}")
