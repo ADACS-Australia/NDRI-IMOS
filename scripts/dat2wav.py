d710ffa356253e25371b9ee4af6005864668322b
MartinCupak <30996725+MartinCupak@users.noreply.github.com>
2024-08-13 15:48:54 +0800

    modif params: add specify output filename and generate output file name

--- scripts/dat2wav.py
+++ scripts/dat2wav.py
@@ -16,14 +16,22 @@ calibration.doWriteIntermediateResults = False
 
 
 def parseArgs():
-    parser = argparse.ArgumentParser()
+    descText = "Convertor for raw IMOS passive audio .DAT record to wav or flac with calibration."
+    parser = argparse.ArgumentParser(description=descText)
     parser.add_argument('--debug', '-d', action='store_true',
                         help='Enable debug mode')
-    parser.add_argument('--filename', '-f', required=True,
-                        help='The name of the raw .DAT file to process.')
-    parser.add_argument('--output', '-o', type=str,
+    parser.add_argument('--input', '-i',
+                        help='The name of the input raw audio .DAT file to process.')
+
+    exGroup = parser.add_mutually_exclusive_group()
+    exGroup.add_argument('--generate-filename', '-g', action='store_true',
+                         help='Generate output filename with setID and time, must provide set ID')
+    exGroup.add_argument('--output', '-o',
+                        help='The name of the output audio file.')
+
+    parser.add_argument('--format', '-f', type=str,
                         choices=['wav', 'flac'], default="wav",
-                        help='Output audio file format (wav, flac)')
+                        help='Format of the output audio file (wav, flac)')
     parser.add_argument('--calibrate', '-c', required=False,
                         help='Calibrate, using calibration file')
     parser.add_argument('--noise', '-n', type=float,
@@ -32,9 +40,18 @@ def parseArgs():
     parser.add_argument('--sensitivity', '-s', type=float,
                         required=False,
                         help='Hydrophone sensitivity (hs)')
-    parser.add_argument('--intermediate', '-i', action='store_true',
+    parser.add_argument('--setID', '-I', type=int,
+                        help='Data set ID')
+    parser.add_argument('--intermediate', '-m', action='store_true',
                         help='Write intermediate results as single column text file')
+
     args = parser.parse_args()
+
+    # Check if --generate-filename was used without --setID
+    if args.generate_filename and args.setID is None:
+        log.error("Parameter --setID (-I) is required when --generate-filename (-g) is used.")
+        parser.error("Parameter --setID (-I) is required when --generate-filename (-g) is used.")
+
     return args
 
 
@@ -52,7 +69,7 @@ if __name__ == "__main__":
                         #  seconds resolution is good enough for logging timestamp
                         datefmt='%Y-%m-%d %H:%M:%S')
 
-    rawFileName = args.filename
+    rawFileName = args.input
     if not os.path.exists(rawFileName):
         log.error(f'Raw dat file {rawFileName} not found!')
         exit(-1)
@@ -70,19 +87,30 @@ if __name__ == "__main__":
             log.warning(f"Hydrophone sensitivity not provided, using default {args.sensitivity}")
 
         if args.intermediate:
-            # calibration.doWriteIntermediateResults is initialised \
+            # calibration.doWriteIntermediateResults is initialised
             # to False in the calibration module
             calibration.doWriteIntermediateResults = True
 
+    if args.setID is not None:
+        setID = args.setID
+    else:
+        setID = 0
+
     binData, numChannels, sampleRate, durationHeader, \
         startTime, endTime = rawdat.readRawFile(rawFileName)
+    
+    durationFile = binData.size / sampleRate
 
-    essentialMetadata = audiofile.MetadataEssential(
+    metadata = audiofile.MetadataFull(
+        setID=setID,
         numChannels=numChannels,
         sampleRate=sampleRate,
         durationHeader=durationHeader,
+        durationFile=durationFile,
         startTime=startTime,
-        endTime=endTime
+        endTime=endTime,
+        calibNoiselevel=args.noise,
+        hydrophoneSensitivity=args.sensitivity
     )
 
     # debugging...
@@ -90,6 +118,18 @@ if __name__ == "__main__":
     log.debug(f"raw .DAT signal type is: {type(binData)}")
     log.debug(f"min bin value in raw .DAT signal size is: {numpy.min(binData)}")
     log.debug(f"max bin value in raw .DAT signal size is: {numpy.max(binData)}")
+
+    # Sort out the output audio file name
+    if args.generate_filename:
+        # created/generated filename has priority
+        outputFileName = audiofile.createOutputFileName(setID,
+                                                        metadata.startTime,
+                                                        args.format)
+    elif args.output is not None:
+        # explicitly specified output filename using commandline parameter
+        outputFileName = args.output
+    else:    
+        outputFileName = audiofile.deriveOutputFileName(rawFileName, args.format) 
 
     numOverloadedSamples = calibration.countOverload(binData)
     if numOverloadedSamples > 0:
@@ -108,7 +148,7 @@ if __name__ == "__main__":
         # calibratedSignal = calibration.calibrate(volts, cnl, hs, calSpec, calFreq, sampleRate)
         calibratedSignal = calibration.calibrateReal(volts, cnl, hs, calSpec, calFreq, sampleRate)
         scaledSignal, scaleFactor = calibration.scale(calibratedSignal)
-        essentialMetadata.scaleFactor = scaleFactor
+        metadata.scaleFactor = scaleFactor
 
         # debugging...
         log.debug(f"scaled calibrated signal size is: {scaledSignal.size}")
@@ -121,37 +161,35 @@ if __name__ == "__main__":
                 scaledSignalInt16 = wav.scaleSignalFloatTo16bitPCM(scaledSignal)
                 numpy.savetxt('signal_scaled.txt', scaledSignalInt16)
 
-            if args.output == 'wav':
+            if args.format == 'wav':
                 # write calibrated wav file with 'audiofile' package library
-                wavFileName = audiofile.deriveOutputFileName(rawFileName, 'wav')
-                audiofile.writeMono16bit(wavFileName, scaledSignal,
-                                         essentialMetadata, 'WAV')
-            elif args.output == 'flac':
+                audiofile.writeMono16bit(outputFileName, scaledSignal,
+                                         metadata, 'WAV')
+            elif args.format == 'flac':
                 # write calibrated flac file with 'audiofile' package library
-                wavFileName = audiofile.deriveOutputFileName(rawFileName, 'flac')
-                audiofile.writeMono16bit(wavFileName, scaledSignal,
-                                         essentialMetadata, 'FLAC')
+                audiofile.writeMono16bit(outputFileName, scaledSignal,
+                                         metadata, 'FLAC')
         else:
             logMsg = "Something went wrong, there is no audio signal data to write to wav file."
             log.error(logMsg)
-    else:
+    else: 
+        # Do not calibrate, just convert the audio record file format 
         if binData is not None:
             # Cannot just save binary data blob to wave,
             # need to convert uint16 to int16
             # Steps: convert to volts, normalise and scale back to signed int16
             volts = calibration.toVolts(binData)
             scaledSignal, scaleFactor = calibration.scale(volts)
-            if args.output == 'wav':
+            metadata.scaleFactor = scaleFactor
+            if args.format == 'wav':
                 scaledSignalInt16 = wav.scaleSignalFloatTo16bitPCM(scaledSignal)
                 # write normalised scaled but still raw uncalibrated data into a wav file
-                wavFileName = wav.deriveWavFileName(rawFileName)
-                wav.writeMono16bit(wavFileName, sampleRate, scaledSignalInt16)
-            elif args.output == 'flac':
-                essentialMetadata.scaleFactor = scaleFactor
+                # intentionally using the 'wave' package function here, not 'audiofile'
+                wav.writeMono16bit(outputFileName, sampleRate, scaledSignalInt16)
+            elif args.format == 'flac':
                 # write calibrated flac file with 'audiofile' package library
-                wavFileName = audiofile.deriveOutputFileName(rawFileName, 'flac')
-                audiofile.writeMono16bit(wavFileName, scaledSignal,
-                                         essentialMetadata, 'FLAC')
+                audiofile.writeMono16bit(outputFileName, scaledSignal,
+                                         metadata, 'FLAC')
         else:
             logMsg = "Something went wrong, there is no audio signal data to write to wav file."
             log.error(logMsg)
